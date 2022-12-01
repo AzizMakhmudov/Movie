b779908c03c3498bb735b63fd1b486aa2aeaedd2
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2022-12-01 18:16:35 +0500

    fixed button add

--- js/main.js
+++ js/main.js
@@ -1,43 +1,88 @@
-var elUl = document.querySelector("[data-ul]");
-var imgUrl = "https://image.tmdb.org/t/p/w500";
-var elForm = document.querySelector("[data-form]");
-var elInputTitle = document.querySelector("[data-input-title]");
-var elInputImg = document.querySelector("[data-input-img]");
+// var elUl = document.querySelector("[data-ul]");
+// var imgUrl = "https://image.tmdb.org/t/p/w500";
+// var elForm = document.querySelector("[data-form]");
+// var elImgUrl = document.querySelector("[data-form-img-url]");
+// var elInputTitle = document.querySelector("[data-form-title]");
 
-renderMovie();
+// renderMovies();
 
-elForm.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  var movie = {
-    title: null,
-    poster_path: null,
-  };
-  elLi.textContent = elInputTitle.value;
-  elSpan.textContent = elInputImg.value;
-  elInputTitle.value = movie.title;
-  elInputImg.value = movie.poster_path;
-  movies.unshift(movie);
-  elUl.prepend(createLi());
-});
+// elForm.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+//   var movie = {
+//     title: null,
+//     poster_path: null,
+//   };
 
-function renderMovie() {
-  for (i = 0; i <= movies.length; i++) {
-    movie = movies[i];
-    elUl.appendChild(createLi(movie));
-  }
-  return elLi;
-}
+//   movie.title = elInputTitle.value;
+//   movie.poster_path = elImgUrl.value;
 
-function createLi(movie) {
-  var elLi = document.createElement("li");
-  var elImg = document.createElement("img");
-  var elSpan = document.createElement("span");
+//   movies.unshift(movie);
 
-  elSpan.textContent = `${movie.title} - ${movie.vote_average}`;
-  elImg.src = `${imgUrl}${movie.poster_path}`;
-  elLi.appendChild(elSpan);
-  elLi.appendChild(elImg);
-  elUl.appendChild(elLi);
+//   renderMovies();
+// });
 
-  return elLi;
-}
+// function renderMovies() {
+//   elUl.innerHTML = "";
+//   for (var i = 0; i < movies.length; i++) {
+//     var movie = movies[i];
+//     var elLi = document.createElement("li");
+//     var elImg = document.createElement("img");
+//     var elSpan = document.createElement("span");
+
+//     elImg.src = `${imgUrl}${movie.poster_path}`;
+//     elImg.width = 100;
+//     elImg.height = 200;
+//     elSpan.textContent = movie.title;
+
+//     elLi.appendChild(elImg);
+//     elLi.appendChild(elSpan);
+
+//     elUl.appendChild(elLi);
+//   }
+// }
+
+// var elUl = document.querySelector("[data-ul]");
+// var imgUrl = "https://image.tmdb.org/t/p/w500";
+// var elForm = document.querySelector("[data-form]");
+// var elImgUrl = document.querySelector("[data-form-img-url]");
+// var elInputTitle = document.querySelector("[data-form-title]");
+
+// renderMovies();
+
+// elForm.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+//   var movie = {
+//     title: null,
+//     poster_path: null,
+//   };
+
+//   movie.title = elInputTitle.value;
+//   movie.poster_path = elImgUrl.value;
+
+//   movies.unshift(movie);
+
+//   elUl.prepend(createLi(movie));
+// });
+
+// function renderMovies() {
+//   elUl.innerHTML = "";
+//   for (var i = 0; i < movies.length; i++) {
+//     var movie = movies[i];
+
+//     elUl.appendChild(createLi(movie));
+//   }
+// }
+// function createLi(movie) {
+//   var elLi = document.createElement("li");
+//   var elImg = document.createElement("img");
+//   var elSpan = document.createElement("span");
+
+//   elImg.src = `${imgUrl}${movie.poster_path}`;
+//   elImg.width = 100;
+//   elImg.height = 200;
+//   elSpan.textContent = movie.title;
+
+//   elLi.appendChild(elImg);
+//   elLi.appendChild(elSpan);
+//   return elLi;
+// }
